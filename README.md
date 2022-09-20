fabc80b6f887d73c21e2e7b6fbf3a6c8593c33bf
herefishyfish <46717769+herefishyfish@users.noreply.github.com>
2022-09-20 18:29:11 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nativescript-stackblitz-templates-ipkbuh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nativescript-stackblitz-templates-ipkbuh)
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core'
+import { Routes } from '@angular/router'
+import { NativeScriptRouterModule } from '@nativescript/angular'
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/items', pathMatch: 'full' },
+]
+
+@NgModule({
+  imports: [NativeScriptRouterModule.forRoot(routes)],
+  exports: [NativeScriptRouterModule],
+})
+export class AppRoutingModule {}
